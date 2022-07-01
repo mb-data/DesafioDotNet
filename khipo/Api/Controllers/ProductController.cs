0253ffb0f019982ce1ecd348347c53a0001cd699
Marcus Costa <marcuskta2@gmail.com>
2022-07-01 19:06:04 +0100

    Start

---
+++ khipo/Api/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private static List<Product> produtos = new List<Product>();
+
+        // GET
+        public List<Product> Get()
+        { 
+            return produtos;
+        }
+
+        // GET ONE
+        public Product GetProdutoId(int id)
+        { 
+            return produtos.FirstOrDefault(x => x.Id == id);
+        }
+
+        // DELETE 
+        public void Delete(int id)
+        {
+            produtos.RemoveAt(produtos.IndexOf(produtos.First(x => x.Equals(id))));
+        }
+
+        // PUT
+        public Product UpdateProduct(int id, Product produto)
+        { 
+            if (id == 0) 
+            {
+                throw new Exception('O valor de ID não pode ser zerado!');
+            }
+            
+            return produtos.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ khipo/Api/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+            
+        public string Brand { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+     }
+
+}
