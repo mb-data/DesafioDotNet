14b54f5cedd20845e76a19d8fdeecd3659c24685
Cleyton <cleytonssantos@yahoo.com.br>
2022-07-30 05:41:55 -0300

    feat: create interface repository

---
+++ ApiDotNetFramework/ApiDotNetFramework/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiDotNetFramework.Interfaces
+{
+    public interface IRepository<TEntity, TKey> where TEntity : class 
+    {
+        List<TEntity> GetAll();
+        TEntity GetById(TKey id);
+        int Insert(TEntity entity);
+        void Update(TEntity entity, TKey id);
+        void Delete(TKey id);
+    }
+}
