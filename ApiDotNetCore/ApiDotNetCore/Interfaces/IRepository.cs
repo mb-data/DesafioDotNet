068f008b09c5af94d5731b35df26828a1fb92f39
Cleyton <cleytonssantos@yahoo.com.br>
2022-07-30 06:29:18 -0300

    feat: create interface repository

---
+++ ApiDotNetCore/ApiDotNetCore/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiDotNetCore.Interfaces
+{
+    public interface IRepository<TEntity, TKey> where TEntity : class
+    {
+        List<TEntity> GetAll();
+        TEntity GetById(TKey id);
+        int Insert(TEntity entity);
+        void Update(TEntity entity, TKey id);
+        void Delete(TKey id);
+    }
+}
