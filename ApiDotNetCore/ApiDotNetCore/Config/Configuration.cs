129e5f4e682667a4a0397162e4220e890a9c10e5
Cleyton <cleytonssantos@yahoo.com.br>
2022-07-31 12:28:54 -0300

    feat: alter connection string to sql server

--- ApiDotNetCore/ApiDotNetCore/Config/Configuration.cs
+++ ApiDotNetCore/ApiDotNetCore/Config/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString { get; set; } = "Data Source=localhost,1433;Initial Catalog=DB_DESAFIO_DOTNET;Integrated Security=False;User Id=sa;Password=1q2w3e4r@;MultipleActiveResultSets=True";
+        public static string ConnectionString { get; set; } = "Data Source=localhost,1433;Initial Catalog=DB_DESAFIO_DOTNET_CORE;Integrated Security=False;User Id=sa;Password=1q2w3e4r@;MultipleActiveResultSets=True";
     }
 }
