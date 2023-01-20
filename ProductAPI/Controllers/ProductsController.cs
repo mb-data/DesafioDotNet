93690508d550fb96e6db476e46076a1b2d0b562a
dihsgarcia <diego.garcia@lis2b.com.br>
2023-01-19 23:51:12 -0300

    Desafio Diego

---
+++ ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Services;
+
+namespace ProductAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _productService.GetAllGetProducts();
+
+            if (products is null)
+                return NotFound("Not found Products in dataBase.");
+
+            return Ok(products);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> GetProductById(int productId)
+        {
+            var products = await _productService.GetProductById(productId);
+
+            if (products is null)
+                return NotFound("Product not found in Database.");
+
+            return Ok(products);
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            var postResult = await _productService.AddProduct(product);
+
+            if (postResult is null)
+                return BadRequest("Product not entered in Database.");
+
+            return Ok(postResult);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(Product request)
+        {
+            var product = await _productService.UpdateProduct(request);
+            if (product is null)
+                return BadRequest("Product not found in Database.");
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> DeleteProduct(int productId)
+        {
+            var delereResult = await _productService.DeleteProduct(productId);
+            if (!delereResult)
+                return BadRequest("Superhero not found in Database.");
+         
+            return Ok($"Product with productId: {productId}, delete from Database");
+
+        }
+
+    }
+}
---
+++ ProductAPI/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data;
+using ProductAPI.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+ConfigureServices(builder.Services);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// add DbContext
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddTransient<IProductService, ProductService>();
+}
